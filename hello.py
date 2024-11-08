85e69dc19f51c913c273ff395f4ba0329d6b1646
shotoyaar <anirudhsridhar1064@gmail.com>
2024-11-08 10:44:35 +0530

    new button

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 import streamlit as st 
 st.write("Hello, you've signed in!")
+st.write("Now, you can play the game using the following command in the command line")
+st.write("<executable> -u <username> -p <password>")
+
+if st.button("Close Window"):
+    close_script = """
+    <script>
+        window.close();
+    </script>
+    """
+    st.markdown(close_script, unsafe_allow_html=True)
