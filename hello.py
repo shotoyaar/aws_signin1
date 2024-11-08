20b15d941a649b8071c8a15293cf29f681173d04
shotoyaar <anirudhsridhar1064@gmail.com>
2024-11-08 10:46:49 +0530

    new button

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
 import streamlit as st 
+import webbrowser
+import streamlit.components as components
+
 st.write("Hello, you've signed in!")
 st.write("Now, you can play the game using the following command in the command line")
 st.write("<executable> -u <username> -p <password>")
 
-if st.button("Close Window"):
-    close_script = """
-    <script>
-        window.close();
-    </script>
-    """
-    st.markdown(close_script, unsafe_allow_html=True)
+def close_window():
+    # JavaScript to close the window
+    js = """
+        <script>
+            window.parent.window.close();
+            // Fallback for browsers that block window.close()
+            window.parent.location.href = "about:blank";
+        </script>
+        """
+    components.html(js, height=0)
+
+# Create a button that triggers the close function
+if st.button("Close Application"):
+    close_window()
